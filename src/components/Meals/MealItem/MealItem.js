3f700bdcd2b078e3204bba78887f8bac025b4d70
Vivek Bhadula <vivekbhadula201420@gmail.com>
2022-01-25 10:33:06 +0530

    changes in input form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
 
 function MealItem(props) {
@@ -10,7 +11,9 @@ function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
